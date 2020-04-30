c51112d12980f5ee7345352f5eef9bbc9b1cf3d2
SCTongYe <wang01@mail.gvsu.edu>
2020-04-30 18:07:37 -0500

    Update jw_pagination.py

--- jw_pagination.py
+++ jw_pagination.py
@@ -5,19 +5,20 @@ Created by @Jiayu Wang on 4/28/20 3:43 PM.
 __author__ = '@JiayuWang'
 
 
+import re
 from django.utils.safestring import mark_safe
 
 
 class JWPagination():
 
-    def __init__(self, page_number, total_customer_count, page_linkpath, per_page_number=10, side_page_number=2):
-  
+    def __init__(self, page_number, total_customer_count, page_linkpath, search_string=None, per_page_number=10, side_page_number=2):
         # PARAMS：
             # page_number = request.GET.get('page', 1)
             # total_customer_count = customer_objs = Customer.objects.all().count()
+            # search_string = request.GET.urlencode()
             # per_page_number = 10
             # side_page_number = 2
-            # page_linkpath = request.path
+            # page_linkpath = 'customers'
 
         try:
             page_number = int(page_number)
@@ -26,12 +27,12 @@ class JWPagination():
         if page_number < 1:
             page_number = 1
 
-
         self.total_customer_count = total_customer_count
         self.page_linkpath = page_linkpath
         self.per_page_number = per_page_number
+        self.search_string = re.sub('page=\d+', '', search_string) if 'page=' in search_string else search_string +'&'
+        print(self.search_string)
 
-        # _q: Quotient r: Remainder 
         _q, _r = divmod(total_customer_count, per_page_number)
 
         if _r:
@@ -40,6 +41,9 @@ class JWPagination():
             total_page_count = _q
 
         self.total_page_count = total_page_count
+
+        if not total_page_count:
+            total_page_count = 1
 
         if page_number > total_page_count:
             page_number = total_page_count
@@ -59,6 +63,7 @@ class JWPagination():
                 start_page_number = 1
 
             end_page_number = total_page_count
+
 
         self.start_page_number = start_page_number
         self.end_page_number = end_page_number
@@ -82,21 +87,18 @@ class JWPagination():
 
         page_pre_html = '<nav aria-label="Page navigation example"><ul class="pagination">'
         if self.start_page_number > 1:
-            page_pre_html = '<nav aria-label="Page navigation example"><ul class="pagination"><li><a href="{0}?page={1}" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>'.format(self.page_linkpath, self.page_number-1)
-
+            page_pre_html = '<nav aria-label="Page navigation example"><ul class="pagination"><li><a href="{0}?{2}page={1}" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>'.format(self.page_linkpath, self.page_number-1, self.search_string)
         pagination_html += page_pre_html
 
         for i in self.total_page_count_range:
             if i == self.page_number:
-                pagination_html += '<li class="active" class="page-item"><a class="page-link" href="{0}?page={1}">{2}</a></li>'.format(self.page_linkpath, i, i)
+                pagination_html += '<li class="active" class="page-item"><a class="page-link" href="{0}?{3}page={1}">{2}</a></li>'.format(self.page_linkpath, i, i, self.search_string)
             else:
-                pagination_html += '<li class="page-item"><a class="page-link" href="{0}?page={1}">{2}</a></li>'.format(self.page_linkpath, i, i)
-
+                pagination_html += '<li class="page-item"><a class="page-link" href="{0}?{3}page={1}">{2}</a></li>'.format(self.page_linkpath, i, i, self.search_string)
         page_next_html = '</ul></nav>'
+        
         if self.end_page_number < self.total_page_count:
-
-            page_next_html = '<li><a href="{0}?page={1}" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li></ul></nav>'.format(self.page_linkpath, self.page_number+1)
-
+            page_next_html = '<li><a href="{0}?{2}page={1}" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li></ul></nav>'.format(self.page_linkpath, self.page_number+1, self.search_string)
         pagination_html += page_next_html
 
         return mark_safe(pagination_html)
